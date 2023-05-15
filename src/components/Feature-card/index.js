f8675b6fc0f3bfea8564f8cac6422cf72bda9310
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2023-05-15 22:24:43 +0100

    update css for card display and home page

--- src/components/Feature-card/index.js
+++ src/components/Feature-card/index.js
@@ -1,28 +1,35 @@
-import { useEffect, useState } from "react"
-import Card from "../Card"
+import { useEffect, useState } from "react";
+import Card from "../Card";
 
-function FeatureCard (){
-  let [featureCard, setFeatureCard] = useState({})
-  
-    useEffect(()=>{
-      async function fetchRandomCard(){
-        let randomNumber = Math.floor(Math.random()*151)
-        const response = await fetch(`https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:${randomNumber}`) 
-        const data = await response.json()
-        let featureCardNumber = Math.floor(Math.random()*(data.data.length))
-        setFeatureCard(data.data[featureCardNumber])
-    }fetchRandomCard()},[])
-    console.log(featureCard)
-return (<>
-  <div>
-  {featureCard.images && featureCard.images.small && (
-          <Card src={featureCard.images.small} alt={featureCard.name} key={featureCard.id} />
+function FeatureCard() {
+  let [featureCard, setFeatureCard] = useState({});
+
+  useEffect(() => {
+    async function fetchRandomCard() {
+      let randomNumber = Math.floor(Math.random() * 151);
+      const response = await fetch(
+        `https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:${randomNumber}`
+      );
+      const data = await response.json();
+      let featureCardNumber = Math.floor(Math.random() * data.data.length);
+      setFeatureCard(data.data[featureCardNumber]);
+    }
+    fetchRandomCard();
+  }, []);
+  console.log(featureCard);
+  return (
+    <>
+      <div>
+        {featureCard.images && featureCard.images.small && (
+          <Card
+            src={featureCard.images.small}
+            alt={featureCard.name}
+            key={featureCard.id}
+          />
         )}
-  </div>
-</>)
+      </div>
+    </>
+  );
 }
 
-
-
-
-export default FeatureCard
+export default FeatureCard;
