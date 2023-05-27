d76597de38922cacf27c4ef1b088c357bde06b28
sam98963 <szwilliamson01@gmail.com>
2023-05-27 13:18:06 +0100

    sort by date function completed - need to trigger a rerender on the cards

--- src/pages/CardCollection/index.js
+++ src/pages/CardCollection/index.js
@@ -16,12 +16,43 @@ function CardCollection() {
       );
       const data = await res.json();
       setPokemon(data.data);
-      console.log(pokemon)
       setIsLoading(false); // set loading state to false once data is loaded
     }
     fetchPokemon();
   }, [pokemonName]);
 
+
+  // function handleSort(e){
+  //   let newOrder = 0
+  //     if(e.target.value === "release-date-asc"){
+  //       newOrder = pokemon.slice().sort((a,b)=>{
+  //        return a.set.releaseDate - b.set.releaseDate
+  //       })
+  //   }
+  //   console.log(newOrder)
+  //   console.log(e.target.value)
+  // }
+
+  function handleSort(e) {
+    let newOrder = [];
+  
+    if (e.target.value === "release-date-asc") {
+      newOrder = pokemon.slice().sort((a, b) => {
+        const releaseDateA = new Date(a.set.releaseDate);
+        const releaseDateB = new Date(b.set.releaseDate);
+        return releaseDateA - releaseDateB;
+      });
+    } else if (e.target.value === "release-date-desc") {
+      newOrder = pokemon.slice().sort((a, b) => {
+        const releaseDateA = new Date(a.set.releaseDate);
+        const releaseDateB = new Date(b.set.releaseDate);
+        return releaseDateB - releaseDateA;
+      });
+  
+    console.log(newOrder);
+    console.log(e.target.value);
+  }}
+  
   return (
     <div className="card-collection">
       {isLoading ? (
@@ -29,6 +60,15 @@ function CardCollection() {
       ) : (
         <>
           <h1>PokeDeck</h1>
+          {/* Select option to pick sort by */}
+          <label>Sort by: <select name= "sort-by" id="sort-by" defaultValue="null" placeholder="null" onChange={e=>handleSort(e)}>
+            <option value="rarity-asc">Rarity (asc)</option>
+            <option value="rarity-desc">Rarity (desc)</option>
+            {/* Release date is data.data[i].set.releaseDate */}
+            <option value="release-date-asc">Date released (oldest)</option>
+            <option value="release-date-desc">Date released (newest)</option>
+          </select></label>
+         
           <div className="card-collection__cards">
             {Array.isArray(pokemon) &&
               pokemon.map((card) => (
@@ -42,3 +82,6 @@ function CardCollection() {
 }
 
 export default CardCollection;
+
+
+// For sorting by Rarity -> set up a switch case that assignes a value to each type of rarity --> Read each value of rarity, then sort by value assigned to each card. Then map again - or trigger a rerender.
